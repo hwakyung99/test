9ba2e391e8d8c649bca9e6603a88c5ba61d7a28e
hwakyung <hwakyung5531@gmail.com>
2022-09-06 15:57:27 +0900

    db uri

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => res.json({message : process.env.PORT}))
+app.get('/', (req, res) => res.json({
+    message : process.env.PORT,
+    db: process.env.MONGO_URI
+}))
 app.listen(process.env.PORT || 80)
