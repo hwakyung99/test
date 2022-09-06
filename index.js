16e808d4bdcb3eacc0e8d8985ca7293b54cb1af1
hwakyung <hwakyung5531@gmail.com>
2022-09-07 01:18:25 +0900

    다시 처음으로

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const express = require('express')
 const app = express()
-const indexRouter = require("./routes")
 
 app.get('/', (req, res) => res.json({
     message : process.env.PORT,
     db: process.env.MONGO_URI
 }))
-
-app.use(morgan("dev"))
-app.use(express.json())
-app.use("/api", indexRouter)
 
 app.listen(process.env.PORT || 80)
